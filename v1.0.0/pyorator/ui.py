7c8bef9140b587cebad1a66abfbb9135568070f9
arisuvade <ariesdave253@gmail.com>
2023-05-12 18:32:39 +0800

    Add theme switch

--- v1.0.0/pyorator/ui.py
+++ v1.0.0/pyorator/ui.py
@@ -4,6 +4,7 @@ from ui.title_label import TitleLabel
 from ui.text_entry import TextEntry
 from ui.language_options import LanguageOptions
 from ui.generate_speech_button import GenerateSpeechButton
+from ui.theme_switch import ThemeSwitch
 from ui.error_label import ErrorLabel
 from pyorator.convert_to_speech import convert_to_speech
 
@@ -23,21 +24,25 @@ class UI(ctk.CTk):
 
         # Text
         self.text_entry = TextEntry(self)
-        self.text_entry.place(relx=0.5, rely=0.17, anchor="center")
+        self.text_entry.place(relx=0.5, rely=0.20, anchor="center")
 
         # Language
         self.language_options = LanguageOptions(self)
-        self.language_options.place(relx=0.5, rely=0.59, anchor="center")
+        self.language_options.place(relx=0.5, rely=0.40, anchor="center")
 
         # Generate speech
         self.generate_speech_button = GenerateSpeechButton(
             self, command=self.convert_to_speech
         )
-        self.generate_speech_button.place(relx=0.5, rely=0.76, anchor="center")
+        self.generate_speech_button.place(relx=0.5, rely=0.60, anchor="center")
+
+        # Theme switch
+        self.theme_switch = ThemeSwitch(self)
+        self.theme_switch.place(relx=0.5, rely=0.80, anchor="center")
 
         # Error
         self.error_label = ErrorLabel(self)
-        self.error_label.place(relx=0.5, rely=0.97, anchor="center")
+        self.error_label.place(relx=0.5, rely=0.92, anchor="center")
 
     def convert_to_speech(self):
         convert_to_speech(
